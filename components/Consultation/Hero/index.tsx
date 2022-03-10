b921dd5203e144caad47e0a2b69c39353f152918
Karol Moroz <k.j.moroz@gmail.com>
2022-03-10 22:26:27 +0100

    Build consultations page with dark bg

--- components/Consultation/Hero/index.tsx
+++ components/Consultation/Hero/index.tsx
@@ -3,16 +3,43 @@ import styles from "./Hero.module.sass";
 
 interface Props {}
 
+const bullets = [
+  "zrozumienie",
+  "wsparcie",
+  "wiedza medyczna",
+  "analiza składu masy ciała",
+  "analiza dotychczasowego jadłospisu",
+  "narzędzia do zmiany nawyków",
+  "jadłospis",
+  "motywacja",
+  "inspiracje kulinarne"
+];
+
 const ConsultationsHero: React.FC<Props> = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.content}>
-        <h1>Was mich nicht umbringt, macht mich stärker.</h1>
+        <h1>Dietetyk kliniczny. Psychodietetyk</h1>
         <p>
           Współpracuję z pacjentami którzy mają za sobą nieudane próby
           odchudzania oraz które w wyniku choroby, zmian życiowych potrzebują
           zmodyfikować swój dotychczasowy jadłospis.
         </p>
+        <ul className={styles.bullets}>
+          {bullets.map((bull, i) => (
+            <li key={i}>
+              <span>{bull}</span>
+            </li>
+          ))}
+        </ul>
+        <footer>
+          <p>Spotkania online lub stacjonarnie.</p>
+          <p>
+            Czas trwania konsultacji: 50 minut
+            <br />
+            Cena konsultacji: 150 ziko
+          </p>
+        </footer>
       </div>
     </section>
   );
